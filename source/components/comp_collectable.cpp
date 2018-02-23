071a4b7e38e0346ba191438d0cbb5537518036a0
JoePerkins <eagoth@gmail.com>
2018-02-23 16:28:26 +0100

    wip

--- source/components/comp_collectable.cpp
+++ source/components/comp_collectable.cpp
@@ -1,24 +1,35 @@
 #include "mcv_platform.h"
 #include "comp_collectable.h"
+#include "entity/entity_parser.h"
 
 DECL_OBJ_MANAGER("collectable", TCompCollectable);
 
 void TCompCollectable::debugInMenu() {
 }
 
-void TCompCollectable::load(const json& j, TEntityParseContext& ctx) {
-
-}
-
-void TCompCollectable::OnCollect(const TMsgTriggerEnter & msg)
-{
-	// Check if collector is the player.
-	CHandle(this).getOwner().destroy();
-}
-
 void TCompCollectable::registerMsgs() {
     DECL_MSG(TCompCollectable, TMsgEntityCreated, onCreate);
+    DECL_MSG(TCompCollectable, TMsgEntitiesGroupCreated, onGroupCreated);
+    DECL_MSG(TCompCollectable, TMsgTriggerEnter, onCollect);
+}
+
+void TCompCollectable::load(const json& j, TEntityParseContext& ctx) {   
+
 }
 
 void TCompCollectable::onCreate(const TMsgEntityCreated& msg) {
+}
+
+void TCompCollectable::onGroupCreated(const TMsgEntitiesGroupCreated & msg) {
+}
+
+void TCompCollectable::onCollect(const TMsgTriggerEnter & msg) {
+    h_collector = msg.h_other_entity;
+    CEntity *collector = h_collector.getOwner();
+    dbg("Trigger enter by %s\n", collector->getName());
+    if (collector->getName() == "The Player") {
+        // Only the player can collect.
+	    CHandle(this).getOwner().destroy();
+    }
+
 }
--- source/components/comp_collectable.h
+++ source/components/comp_collectable.h
@@ -8,13 +8,14 @@ class TCompCollectable: public TCompBase {
 	DECL_SIBLING_ACCESS();
 
 	float collision_radius = 0.3f;
-
-	void OnCollect(const TMsgTriggerEnter& msg);
+    CHandle         h_collector;
 
 public:
-
     void debugInMenu();
     static void registerMsgs();
     void load(const json& j, TEntityParseContext& ctx);
+
     void onCreate(const TMsgEntityCreated& msg);
+    void onGroupCreated(const TMsgEntitiesGroupCreated& msg);
+    void onCollect(const TMsgTriggerEnter& msg);
 };
