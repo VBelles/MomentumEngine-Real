0bb9f681a8af6fe61204a4377b26aaba61896d52
JoePerkins <eagoth@gmail.com>
2018-03-13 13:27:29 +0100

    microlimpieza

--- source/components/comp_render.h
+++ source/components/comp_render.h
@@ -39,8 +39,6 @@ public:
     void load(const json& j, TEntityParseContext& ctx);
 
     void setMesh(std::string meshName, std::string materialName = "");
-    const CRenderMesh* getMesh() { return mesh; }
-    const CMaterial* getMaterial() { return material; }
 
     void update(float delta);
     void TurnRed(float time);
