4951cbed2e743e28be40a4934a82fcff023dc6fa
VBelles <vicentbelles@gmail.com>
2018-04-07 20:41:56 +0200

    breakable breaking

--- source/components/comp_breakable.cpp
+++ source/components/comp_breakable.cpp
@@ -10,21 +10,41 @@ void TCompBreakable::debugInMenu() {
 void TCompBreakable::registerMsgs() {
 	DECL_MSG(TCompBreakable, TMsgEntitiesGroupCreated, onGroupCreated);
 	DECL_MSG(TCompBreakable, TMsgColliderDestroyed, onColliderDestroyed);
+	DECL_MSG(TCompBreakable, TMsgAttackHit, onHit);
 }
 
 void TCompBreakable::load(const json& j, TEntityParseContext& ctx) {
-	
+	hp = j.value("hp", 1.f);
 }
 
 void TCompBreakable::onGroupCreated(const TMsgEntitiesGroupCreated & msg) {
+	colliderHandle = get<TCompCollider>();
 }
 
 void TCompBreakable::update(float dt) {
+
+}
+
+void TCompBreakable::onHit(const TMsgAttackHit & msg) {
+	hp -= 1;
+	if (hp <= 0) {
+		onDie();
+	}
+}
+
+void TCompBreakable::onDie() {
+	getCollider()->destroy();
+}
+
+void TCompBreakable::dropLoot() {
 	
 }
 
-
-
 void TCompBreakable::onColliderDestroyed(const TMsgColliderDestroyed& msg) {
+	dropLoot();
 	CHandle(this).getOwner().destroy();
 }
+
+
+
+
--- source/components/comp_breakable.h
+++ source/components/comp_breakable.h
@@ -8,19 +8,29 @@
 class TCompBreakable : public TCompBase {
 	DECL_SIBLING_ACCESS();
 
-public:
-
 private:
-	
+	CHandle colliderHandle;
+	float hp = 0.f;
+
+	//Messages
+	void onGroupCreated(const TMsgEntitiesGroupCreated& msg);
+	void onColliderDestroyed(const TMsgColliderDestroyed& msg);
+	void onHit(const TMsgAttackHit& msg);
+
+	void onDie();
+	void dropLoot();
+
+	TCompCollider* getCollider() { return colliderHandle; }
+
 public:
 
-	void debugInMenu();
 	static void registerMsgs();
+	void debugInMenu();
 	void load(const json& j, TEntityParseContext& ctx);
-
-	void onGroupCreated(const TMsgEntitiesGroupCreated& msg);
 	void update(float dt);
 
-	void onColliderDestroyed(const TMsgColliderDestroyed& msg);
+	
+
+	
 
 };
