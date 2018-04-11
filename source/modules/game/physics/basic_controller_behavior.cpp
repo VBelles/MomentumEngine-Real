396cc775c67bcbb892e7e040553f2d7edb9052dd
VBelles <vicentbelles@gmail.com>
2018-04-11 15:27:45 +0200

    las plataformas rotativas funcionan y hacen resvalar al player

--- source/modules/game/physics/basic_controller_behavior.cpp
+++ source/modules/game/physics/basic_controller_behavior.cpp
@@ -1,8 +1,16 @@
 #include "mcv_platform.h"
 #include "basic_controller_behavior.h"
 #include "entity/common_msgs.h"
+#include "components/platforms/comp_platform_rotative.h"
 
 PxControllerBehaviorFlags BasicControllerBehavior::getBehaviorFlags(const PxShape & shape, const PxActor & actor) {
+	CHandle colliderHandle;
+	colliderHandle.fromVoidPtr(actor.userData);
+	CEntity* entity = colliderHandle.getOwner();
+	TCompPlatformRotative* platformRotative = entity->get<TCompPlatformRotative>();
+	if (platformRotative) {
+		return PxControllerBehaviorFlag::eCCT_SLIDE;
+	}
 	return PxControllerBehaviorFlag::eCCT_USER_DEFINED_RIDE;
 }
 
