db43f3e16a73f26347409fdf0d3324cefc048266
JoePerkins <eagoth@gmail.com>
2018-04-16 14:03:36 +0200

    Borrar lua viejo, arreglar SLB para que compile (estaba con SDK viejo).

--- source/main.cpp
+++ source/main.cpp
@@ -1,15 +1,7 @@
 #include "mcv_platform.h"
 #include "windows/app.h"
 
-//extern "C" {
-//#include "lua/lua.h"
-//#include "lua/lualib.h"
-//#include "lua/lauxlib.h"
-//}
-
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
-    //lua_State* ls = luaL_newstate();
-
     CApp app;
 
     if (!app.readConfig())
