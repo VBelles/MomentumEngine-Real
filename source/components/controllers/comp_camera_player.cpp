1881a455aaf489a9b64f36ca19e965b018b5aaf3
VBelles <vicentbelles@gmail.com>
2018-05-05 21:07:19 +0200

    La camara no entra en las trimeshes ahora

--- source/components/controllers/comp_camera_player.cpp
+++ source/components/controllers/comp_camera_player.cpp
@@ -64,7 +64,7 @@ void TCompCameraPlayer::update(float delta) {
 		updateMovement(delta);
 	}
 
-	if (!isMovementLocked && sphereCast()) {
+	if (!isMovementLocked && (sphereCast() || isCameraInsideGeometry())) {
 		sweepBack();
 	}
 }
@@ -179,6 +179,7 @@ void TCompCameraPlayer::sweepBack() {
 	}
 }
 
+
 bool TCompCameraPlayer::sphereCast() {
 	PxSphereGeometry sphereShape(sphereCastRadius); //shape to test for overlaps
 	PxTransform pxTransform(toPhysx(getTransform()));
@@ -189,6 +190,33 @@ bool TCompCameraPlayer::sphereCast() {
 	bool status = EnginePhysics.getScene()->overlap(sphereShape, pxTransform, buf, fd, EnginePhysics.getGameQueryFilterCallback());
 	return status;
 }
+
+bool TCompCameraPlayer::isCameraInsideGeometry() {
+	VEC3 cameraPosition = getTransform()->getPosition();
+	VEC3 targetPosition = targetTransform.getPosition();
+
+	PxRaycastBuffer buf1;
+	raycast(targetPosition, cameraPosition, buf1);
+	PxRaycastBuffer buf2;
+	raycast(cameraPosition, targetPosition, buf2);
+
+	dbg("Buf1: %d\n", buf1.getNbAnyHits());
+	dbg("Buf2: %d\n", buf2.getNbAnyHits());
+
+	return buf1.getNbAnyHits() != buf2.getNbAnyHits();
+}
+
+bool TCompCameraPlayer::raycast(VEC3 origin, VEC3 destination, PxRaycastCallback& callback) {
+	VEC3 dir = destination - origin;
+	dir.Normalize();
+	float distance = VEC3::Distance(origin, destination);
+	PxHitFlags hitflags = PxHitFlags(PxHitFlag::eDEFAULT);
+	PxQueryFilterData filterData(PxFilterData(EnginePhysics.Player, EnginePhysics.Scenario, 0, 0),
+		PxQueryFlag::eDYNAMIC | PxQueryFlag::eSTATIC | PxQueryFlag::eANY_HIT | PxQueryFlag::ePREFILTER);
+	bool status = EnginePhysics.getScene()->raycast(toPhysx(origin), toPhysx(dir), distance, callback, hitflags, filterData, EnginePhysics.getGameQueryFilterCallback());
+	return status;
+}
+
 
 void TCompCameraPlayer::centerCamera() {
 	if (!isMovementLocked) {
