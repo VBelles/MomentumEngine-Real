d8f5ef286b74af6cd9e0ad964192054b486b320b
VBelles <vicentbelles@gmail.com>
2018-05-11 02:18:13 +0200

    Se pueden cargar bancos, en teoria

--- source/modules/system/module_sound.cpp
+++ source/modules/system/module_sound.cpp
@@ -13,12 +13,25 @@ bool CModuleSound::start() {
 	FMOD_RESULT res;
 	res = Studio::System::create(&system);
 	res = system->initialize(1024, FMOD_STUDIO_INIT_NORMAL, FMOD_INIT_NORMAL, extraDriverData);
+
+	json j = loadJson("data/sounds.json");
+	for (std::string bankFile : j["banks"]) {
+		Studio::Bank* bank = nullptr;
+		res = system->loadBankFile(bankFile.c_str(), FMOD_STUDIO_LOAD_BANK_NORMAL, &bank);
+		assert(res == 0);
+		banks[bankFile] = bank;
+	}
+
 	return true;
 }
 
 bool CModuleSound::stop() {
+	for (auto& p : banks) {
+		p.second->unload();
+	}
 	FMOD_RESULT res;
 	res = system->release();
+	banks.clear();
 	return true;
 }
 
--- source/modules/system/module_sound.h
+++ source/modules/system/module_sound.h
@@ -8,6 +8,8 @@ private:
 	FMOD::System* lowLevelSystem = nullptr;
 	FMOD::Studio::System* system = nullptr;
 
+	std::map<std::string, FMOD::Studio::Bank*> banks;
+
 public:
 	CModuleSound(const std::string& name);
 	bool start() override;
