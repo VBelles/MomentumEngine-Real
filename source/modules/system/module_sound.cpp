81c4a88e2835e8301299ddea1eb02aa1eaad6937
VBelles <vicentbelles@gmail.com>
2018-05-11 18:38:49 +0200

    FMOD integrado y funcionando TODO: posibilidad de varias instancias de un mismo evento (sin ser emiters)

--- source/modules/system/module_sound.cpp
+++ source/modules/system/module_sound.cpp
@@ -10,15 +10,29 @@ CModuleSound::CModuleSound(const std::string& name) : IModule(name) {
 }
 
 bool CModuleSound::start() {
-	FMOD_RESULT res;
 	res = Studio::System::create(&system);
 	res = system->initialize(1024, FMOD_STUDIO_INIT_NORMAL, FMOD_INIT_NORMAL, extraDriverData);
+
+	const int maxLen = 255;
+	char buffer[maxLen + 1];
 
 	json j = loadJson("data/sounds.json");
 	for (std::string bankFile : j["banks"]) {
 		Studio::Bank* bank = nullptr;
 		res = system->loadBankFile(bankFile.c_str(), FMOD_STUDIO_LOAD_BANK_NORMAL, &bank);
-		assert(res == 0);
+		assert(res == FMOD_OK);
+		int eventCount = 0;
+		bank->getEventCount(&eventCount);
+		if (!eventCount) continue;
+		std::vector<Studio::EventDescription*> eventsDescription(eventCount);
+		bank->getEventList(&eventsDescription[0], eventCount, &eventCount);
+		for (auto event : eventsDescription) {
+			std::string eventPath;
+			event->getPath(&eventPath[0], maxLen, 0);
+
+			dbg("%s\n", eventPath.c_str());
+
+		}
 		banks[bankFile] = bank;
 	}
 
@@ -38,5 +52,56 @@ bool CModuleSound::stop() {
 void CModuleSound::update(float delta) {
 	system->update();
 }
+
+void CModuleSound::instanceEvent(const char* event) {
+	if (eventInstances.find(event) == eventInstances.end()) {
+		Studio::EventDescription* descriptor = nullptr;
+		res = system->getEvent(event, &descriptor);
+		Studio::EventInstance* eventInstance = nullptr;
+		res = descriptor->createInstance(&eventInstance);
+		eventInstances[event] = eventInstance;
+	}
+	else {
+		dbg("Event '%s' already instanced\n", event);
+	}
+}
+
+void CModuleSound::releaseEvent(const char* event) {
+	try {
+		eventInstances.at(event)->release();
+		eventInstances.erase(event);
+	}
+	catch (const std::out_of_range& e) {
+		dbg("Releasing an uninstanced event\n");
+	}
+}
+
+void CModuleSound::startEvent(const char* event) {
+	try {
+		eventInstances.at(event)->start();
+	}
+	catch (const std::out_of_range& e) {
+		dbg("Starting an uninstanced event\n");
+	}
+}
+
+void CModuleSound::stopEvent(const char* event, FMOD_STUDIO_STOP_MODE mode) {
+	try {
+		eventInstances.at(event)->stop(mode);
+	}
+	catch (const std::out_of_range& e) {
+		dbg("Stoping an uninstanced event\n");
+	}
+}
+
+void CModuleSound::emitEvent(const char* event) {
+	Studio::EventDescription* descriptor = nullptr;
+	res = system->getEvent(event, &descriptor);
+	Studio::EventInstance* eventInstance = nullptr;
+	res = descriptor->createInstance(&eventInstance);
+	eventInstance->start();
+	eventInstance->release();
+}
+
 
 
--- source/modules/system/module_sound.h
+++ source/modules/system/module_sound.h
@@ -2,19 +2,31 @@
 
 #include "modules/module.h"
 
-class CModuleSound : public IModule {
+#define TEST_EVENT "event:/onTest"
+
+class CModuleSound : public IModule {	
 private:
+	FMOD_RESULT res;
 	void *extraDriverData = nullptr;
 	FMOD::System* lowLevelSystem = nullptr;
 	FMOD::Studio::System* system = nullptr;
 
 	std::map<std::string, FMOD::Studio::Bank*> banks;
+	std::map<std::string, FMOD::Studio::EventInstance*> eventInstances;
 
 public:
 	CModuleSound(const std::string& name);
 	bool start() override;
 	bool stop() override;
 	void update(float delta) override;
+
+	void instanceEvent(const char * event);
+	void releaseEvent(const char * event);
+
+	void startEvent(const char * event);
+	void stopEvent(const char * event, FMOD_STUDIO_STOP_MODE mode = FMOD_STUDIO_STOP_IMMEDIATE);
+
+	void emitEvent(const char * event);
 
 };
 
