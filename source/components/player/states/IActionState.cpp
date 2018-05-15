a2a98741d1ef19f0c055a779845791e14b59a152
VBelles <vicentbelles@gmail.com>
2018-05-16 01:48:35 +0200

    soluciona el bug de la rampa en debug

--- source/components/player/states/IActionState.cpp
+++ source/components/player/states/IActionState.cpp
@@ -116,6 +116,9 @@ void IActionState::clampHorizontalVelocity(float maxHorizontalSpeed) {
 }
 
 bool IActionState::isWalkable(MoveState& moveState) {
+	if (moveState.botHits.empty()) {
+		return true;
+	}
 	for (HitState& hit : moveState.botHits) {
 		if (hit.dotUp >= getPlayerModel()->getController()->getSlopeLimit()) {
 			return true;
