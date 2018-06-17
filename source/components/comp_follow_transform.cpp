ef8450de677f3ab516ed6c5f67016ab7e9480857
Daniel Tomas <ticidici@yahoo.es>
2018-06-18 01:33:27 +0200

    cosas imgui luz

--- source/components/comp_follow_transform.cpp
+++ source/components/comp_follow_transform.cpp
@@ -7,8 +7,9 @@
 
 DECL_OBJ_MANAGER("follow_transform", TCompFollowTransform);
 
-void TCompFollowTransform::registerMsgs() {
-	DECL_MSG(TCompFollowTransform, TMsgAllScenesCreated, onAllScenesCreated);
+void TCompFollowTransform::debugInMenu() {
+	ImGui::DragFloat3("offset", &offset.x, 0.5f, -1000.f, 1000.f);
+
 }
 
 void TCompFollowTransform::load(const json& j, TEntityParseContext& ctx) {
@@ -16,14 +17,17 @@ void TCompFollowTransform::load(const json& j, TEntityParseContext& ctx) {
 	offset = loadVEC3(j["offset"]);
 }
 
+void TCompFollowTransform::registerMsgs() {
+	DECL_MSG(TCompFollowTransform, TMsgAllScenesCreated, onAllScenesCreated);
+}
+
+
 void TCompFollowTransform::onAllScenesCreated(const TMsgAllScenesCreated& msg) {
 	CEntity* otherEntity = getEntityByName(entityName);
 	followingTransformHandle = otherEntity->get<TCompTransform>();
 	transformHandle = get<TCompTransform>();
 }
 
-void TCompFollowTransform::debugInMenu() {
-}
 
 void TCompFollowTransform::update(float dt) {
 	getTransform()->setPosition(getFollowingTransform()->getPosition() + offset);
