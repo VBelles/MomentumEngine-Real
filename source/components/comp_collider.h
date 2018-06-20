48e0b2c5259edcccb7fe1c1b2c213f6d5e9dec80
VBelles <vicentbelles@gmail.com>
2018-06-20 17:51:22 +0200

    El cct behavior de los enemigos separados del del player

--- source/components/comp_collider.h
+++ source/components/comp_collider.h
@@ -2,10 +2,10 @@
 
 #include "comp_base.h"
 #include "entity/msgs.h"
+#include "modules/game_modules/physics/basic_controller_behavior.h"
 
 class CCollisionMesh;
 struct TMsgEntityCreated;
-
 
 struct TMsgTriggerEnter {
 	CHandle h_other_entity;
@@ -35,19 +35,20 @@ struct ColliderConfig {
 	unsigned int mask;
 	bool isTrigger;
 
-    const CCollisionMesh* colMesh = nullptr;
+	const CCollisionMesh* colMesh = nullptr;
 };
 
 class TCompCollider : public TCompBase {
-	DECL_SIBLING_ACCESS();
-
 private:
 	bool created = false;
 	void setCreated(bool value) { created = value; }
 public:
+	DECL_SIBLING_ACCESS();
+
 	ColliderConfig config;
-	PxActor* actor;
-	PxController* controller;
+	PxActor* actor = nullptr;
+	PxController* controller = nullptr;
+	BasicControllerBehavior* controllerBehavior = nullptr;
 
 	//Parent methods
 	static void registerMsgs();
@@ -62,6 +63,5 @@ public:
 	void disableSceneQuery();
 	bool isCreated() { return created; }
 	void setupFiltering(PxU32 filterGroup, PxU32 filterMask);
-
 };
 
--- source/modules/game_modules/physics/basic_controller_behavior.h
+++ source/modules/game_modules/physics/basic_controller_behavior.h
@@ -1,7 +1,15 @@
 #pragma once
 
 class BasicControllerBehavior : public PxControllerBehaviorCallback {
+private:
+	CHandle entityHandle;
+
+public:
+	BasicControllerBehavior(CHandle entityHandle);
+
 	virtual PxControllerBehaviorFlags getBehaviorFlags(const PxShape& shape, const PxActor& actor) override;
 	virtual PxControllerBehaviorFlags getBehaviorFlags(const PxController& controller) override;
 	virtual PxControllerBehaviorFlags getBehaviorFlags(const PxObstacle& obstacle) override;
+
+	virtual ~BasicControllerBehavior() override {};
 };
