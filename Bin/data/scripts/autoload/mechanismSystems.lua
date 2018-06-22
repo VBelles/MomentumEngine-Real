9ab9fbad6925e0249acc216c16b047940ec95725
VBelles <vicentbelles@gmail.com>
2018-06-22 20:08:07 +0200

    Añadir parámetro appear a los mecanismos. Habilitar funcionamiento de dos mecanismos más. Añadir cosas ricas en la torre.

--- Bin/data/scripts/autoload/mechanismSystems.lua
+++ Bin/data/scripts/autoload/mechanismSystems.lua
@@ -1,3 +1,115 @@
+--~ MILESTONE 3 ~--
+
+--Mech001
+function onMechanismSystemActivated_ms3_mech001_Box332()
+	closeDoor("ms3_mech001_Box332", 0)
+end
+function onMechanismSystemDeactivated_ms3_mech001_Box332()
+	openDoor("ms3_mech001_Box332", 0)
+end
+
+function onMechanismSystemActivated_ms3_mech001_Box333()
+	closeDoor("ms3_mech001_Box333", 0)
+end
+function onMechanismSystemDeactivated_ms3_mech001_Box333()
+	openDoor("ms3_mech001_Box333", 0)
+end
+
+--Mech004
+function onMechanismSystemActivated_ms3_mech004_platf173()
+	closeDoor("ms3_mech004_platf173", 0)
+end
+function onMechanismSystemDeactivated_ms3_mech004_platf173()
+	openDoor("ms3_mech004_platf173", 0)
+end
+
+function onMechanismSystemActivated_ms3_mech004_platf174()
+	closeDoor("ms3_mech004_platf174", 0)
+end
+function onMechanismSystemDeactivated_ms3_mech004_platf174()
+	openDoor("ms3_mech004_platf174", 0)
+end
+
+function onMechanismSystemActivated_ms3_mech004_platf175()
+	closeDoor("ms3_mech004_platf175", 0)
+end
+function onMechanismSystemDeactivated_ms3_mech004_platf175()
+	openDoor("ms3_mech004_platf175", 0)
+end
+
+function onMechanismSystemActivated_ms3_mech004_platf176()
+	closeDoor("ms3_mech004_platf176", 0)
+end
+function onMechanismSystemDeactivated_ms3_mech004_platf176()
+	openDoor("ms3_mech004_platf176", 0)
+end
+
+function onMechanismSystemActivated_ms3_mech004_platf178()
+	closeDoor("ms3_mech004_platf178", 0)
+end
+function onMechanismSystemDeactivated_ms3_mech004_platf178()
+	openDoor("ms3_mech004_platf178", 0)
+end
+
+function onMechanismSystemActivated_ms3_mech004_platf179()
+	closeDoor("ms3_mech004_platf179", 0)
+end
+function onMechanismSystemDeactivated_ms3_mech004_platf179()
+	openDoor("ms3_mech004_platf179", 0)
+end
+
+function onMechanismSystemActivated_ms3_mech004_platf181()
+	closeDoor("ms3_mech004_platf181", 0)
+end
+function onMechanismSystemDeactivated_ms3_mech004_platf181()
+	openDoor("ms3_mech004_platf181", 0)
+end
+
+--Mech005
+function onMechanismSystemActivated_ms3_mech005_platf182()
+	closeDoor("ms3_mech005_platf182", 0)
+end
+function onMechanismSystemDeactivated_ms3_mech005_platf182()
+	openDoor("ms3_mech005_platf182", 0)
+end
+
+function onMechanismSystemActivated_ms3_mech005_platf183()
+	closeDoor("ms3_mech005_platf183", 0)
+end
+function onMechanismSystemDeactivated_ms3_mech005_platf183()
+	openDoor("ms3_mech005_platf183", 0)
+end
+
+function onMechanismSystemActivated_ms3_mech005_platf184()
+	closeDoor("ms3_mech005_platf184", 0)
+end
+function onMechanismSystemDeactivated_ms3_mech005_platf184()
+	openDoor("ms3_mech005_platf184", 0)
+end
+
+function onMechanismSystemActivated_ms3_mech005_platf188()
+	closeDoor("ms3_mech005_platf188", 0)
+end
+function onMechanismSystemDeactivated_ms3_mech005_platf188()
+	openDoor("ms3_mech005_platf188", 0)
+end
+
+function onMechanismSystemActivated_ms3_mech005_platf190()
+	closeDoor("ms3_mech005_platf190", 0)
+end
+function onMechanismSystemDeactivated_ms3_mech005_platf190()
+	openDoor("ms3_mech005_platf190", 0)
+end
+
+function onMechanismSystemActivated_ms3_mech005_platf192()
+	closeDoor("ms3_mech005_platf192", 0)
+end
+function onMechanismSystemDeactivated_ms3_mech005_platf192()
+	openDoor("ms3_mech005_platf192", 0)
+end
+
+--~ MILESTONE 2 ~--
+
 function onMechanismSystemActivated_ms2_mech006_appear001mesh()
 	openDoor("ms2_mech006_appear001mesh", 0)
 end
@@ -66,19 +178,4 @@ function onMechanismSystemActivated_ms2_mech005_appear001mesh()
 end
 function onMechanismSystemDeactivated_ms2_mech005_appear001mesh()
 	openDoor("ms2_mech005_appear001mesh", 0)
-end
-
-function onMechanismSystemActivated_ms3_mech001_Box332()
-	closeDoor("ms3_mech001_Box332", 0)
-end
-function onMechanismSystemDeactivated_ms3_mech001_Box332()
-	openDoor("ms3_mech001_Box332", 0)
-end
-
---~ MILESTONE 3 ~--
-function onMechanismSystemActivated_ms3_mech001_Box333()
-	closeDoor("ms3_mech001_Box333", 0)
-end
-function onMechanismSystemDeactivated_ms3_mech001_Box333()
-	openDoor("ms3_mech001_Box333", 0)
 end
