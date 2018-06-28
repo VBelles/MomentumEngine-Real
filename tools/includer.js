602bcbbe8697c4b12fdeee2001aa015a9b8ebbf1
VBelles <vicentbelles@gmail.com>
2018-06-28 17:08:44 +0200

    includer sync

--- tools/includer.js
+++ tools/includer.js
@@ -6,25 +6,25 @@ const iaIncludePath = '#include "components/ia/enemies/common/';
 
 let includes = [];
 
-search(searchPath, iaIncludePath);
-function search(folder, includePath) {
-    fs.readdir(folder, function (err, files) {
-        files.forEach(function (file, index) {
-            let fromPath = path.join(folder, file);
-            fs.stat(fromPath, function (error, stat) {
-                if (stat.isFile()) {
-                    if (path.extname(file) == '.h') {
-                        includes.push(includePath + file + '"');
-                    }
-                } else if (stat.isDirectory()) {
-                    search(fromPath, includePath + file + '/');
-                }
-            });
-        });
-    });
+searchSync(searchPath, iaIncludePath);
 
-    includes.forEach(value => {
-        console.log(value);
+includes.forEach(include =>{
+    console.log(include);
+});
+
+function searchSync(folder, includePath){
+    const files = fs.readdirSync(folder).sort();
+    console.log(files);
+    files.forEach(file =>{
+        let absolutePath = path.join(folder, file);
+        const stat = fs.statSync(absolutePath);
+        if(stat.isFile()){
+            if(path.extname(file) === '.h'){
+                includes.push(includePath + file + '"');
+            }
+        }
+        else if(stat.isDirectory()){
+            searchSync(absolutePath, includePath + file + '/');
+        }
     });
 }
-
