16fd952b3718c453443054a6a9a60162a5fa55af
VBelles <vicentbelles@gmail.com>
2018-07-12 13:11:13 +0200

    Ahora compila lo del respawn transform.

--- source/components/comp_respawn_point.cpp
+++ source/components/comp_respawn_point.cpp
@@ -16,10 +16,10 @@ void TCompRespawnPoint::load(const json& j, TEntityParseContext& ctx) {
 	if (j.count("respawn_position")) {
 		respawnPosition = loadVEC3(j["respawn_position"]);
 		if (j.count("respawn_rotation")) {
-			TCompTransform* transform;
-			transform->setRotation(loadQUAT(j["respawn_rotation"]));
+			CTransform transform = CTransform();
+			transform.setRotation(loadQUAT(j["respawn_rotation"]));
 			float pitch;
-			transform->getYawPitchRoll(&respawnYaw, &pitch);
+			transform.getYawPitchRoll(&respawnYaw, &pitch);
 		}
 		else {
 			respawnYaw = j.value("respawn_yaw", respawnYaw);
