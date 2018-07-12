57158ea4ca79febb782ab4f5480664f143103610
ticidici <daniel.tomas@alum.upf.edu>
2018-07-12 16:49:45 +0200

    altar ya sólo espawnea una vez

--- source/components/comp_altar.cpp
+++ source/components/comp_altar.cpp
@@ -58,12 +58,14 @@ void TCompAltar::onHit(const TMsgAttackHit & msg) {
 			anims->setStopOnNextLoop(true);
 		}
 		EngineScripting.throwEvent(onAltarDestroyed, ((CEntity*)CHandle(this).getOwner())->getName());
-
-		//Activar las crisálidas aquí, para poderlo integrar con 3dmax
-		for (VEC3 spawnPos : spawnPositions) {
-			TEntityParseContext ctx;
-			ctx.root_transform.setPosition(spawnPos);
-			parseScene(prefabToSpawn, ctx);
+		if (!isActivated) {
+			isActivated = true;
+			//Activar las crisálidas aquí, para poderlo integrar con 3dmax
+			for (VEC3 spawnPos : spawnPositions) {
+				TEntityParseContext ctx;
+				ctx.root_transform.setPosition(spawnPos);
+				parseScene(prefabToSpawn, ctx);
+			}
 		}
 	}
 }
