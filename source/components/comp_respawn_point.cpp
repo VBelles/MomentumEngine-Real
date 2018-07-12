aa11c3f9f5606a78425977dcc3ad5b6bd7bcec10
ticidici <daniel.tomas@alum.upf.edu>
2018-07-12 12:56:42 +0200

    respawn point coge toda la rotación porque néstor es tonta

--- source/components/comp_respawn_point.cpp
+++ source/components/comp_respawn_point.cpp
@@ -1,6 +1,7 @@
 #include "mcv_platform.h"
 #include "comp_respawn_point.h"
 #include "components/comp_collider.h"
+#include "components/comp_transform.h"
 
 DECL_OBJ_MANAGER("respawn_point", TCompRespawnPoint);
 
@@ -14,7 +15,15 @@ void TCompRespawnPoint::registerMsgs() {
 void TCompRespawnPoint::load(const json& j, TEntityParseContext& ctx) {
 	if (j.count("respawn_position")) {
 		respawnPosition = loadVEC3(j["respawn_position"]);
-		respawnYaw = j.value("respawn_yaw", respawnYaw);
+		if (j.count("respawn_rotation")) {
+			TCompTransform* transform;
+			transform->setRotation(loadQUAT(j["respawn_rotation"]));
+			float pitch;
+			transform->getYawPitchRoll(&respawnYaw, &pitch);
+		}
+		else {
+			respawnYaw = j.value("respawn_yaw", respawnYaw);
+		}
 	}
 }
 
