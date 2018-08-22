c6b4c397c3c98d5bf2faeed655545565ea6010b1
José Manuel Toledo <jose24@outlook.com>
2018-08-22 15:24:17 +0200

    Se ve algo

--- source/gui/controllers/hud_controller.cpp
+++ source/gui/controllers/hud_controller.cpp
@@ -2,6 +2,8 @@
 #include "hud_controller.h"
 #include "gui/widgets/gui_button.h"
 #include "gui/widgets/gui_text.h"
+#include "components/player/comp_player_model.h"
+#include "components/player/comp_power_gauge.h"
 
 namespace GUI {
 	CHudController::CHudController() {
@@ -10,7 +12,35 @@ namespace GUI {
 			EngineGUI.getVariables().setVariant(jData);
 		}
 	}
-	void CHudController::update(float delta) {
 
+	void CHudController::update(float delta) {
+		TCompPlayerModel* playermodel = ((CEntity*)getEntityByName(PLAYER_NAME))->get<TCompPlayerModel>();
+		TCompPowerGauge* powerGauge = playermodel->getPowerGauge();
+
+		EngineGUI.getVariables().getVariant("hp")->setInt(playermodel->getHp());
+		EngineGUI.getVariables().getVariant("hp_progress")->setFloat(playermodel->getHp() / playermodel->getMaxHp());
+
+		int coinBars = playermodel->getNumberOfCoins() * playermodel->getPowerPerCoin() / powerGauge->getMaxPower();
+		int coinsPerBar = powerGauge->getMaxPower() / playermodel->getPowerPerCoin();
+		float coinProgress = playermodel->getNumberOfCoins() / (float)coinsPerBar - coinBars;
+		EngineGUI.getVariables().getVariant("coin_bars")->setInt(coinBars);
+		EngineGUI.getVariables().getVariant("coin_progress")->setFloat(coinProgress);
+
+		//EngineGUI.getVariables().getVariant("power_progress")->setFloat(powerGauge->getBarPercentage());
+		if (powerGauge->getPowerLevel() == 1) {
+			EngineGUI.getVariables().getVariant("power_progress_level1")->setFloat(powerGauge->getPowerLevelPercentage());
+			EngineGUI.getVariables().getVariant("power_progress_level2")->setFloat(0);
+			EngineGUI.getVariables().getVariant("power_progress_level3")->setFloat(0);
+		}
+		else if (playermodel->getPowerGauge()->getPowerLevel() == 2) {
+			EngineGUI.getVariables().getVariant("power_progress_level1")->setFloat(1);
+			EngineGUI.getVariables().getVariant("power_progress_level2")->setFloat(powerGauge->getPowerLevelPercentage());
+			EngineGUI.getVariables().getVariant("power_progress_level3")->setFloat(0);
+		}
+		else if (playermodel->getPowerGauge()->getPowerLevel() == 3) {
+			EngineGUI.getVariables().getVariant("power_progress_level1")->setFloat(1);
+			EngineGUI.getVariables().getVariant("power_progress_level2")->setFloat(1);
+			EngineGUI.getVariables().getVariant("power_progress_level3")->setFloat(powerGauge->getPowerLevelPercentage());
+		}
 	}
 }
