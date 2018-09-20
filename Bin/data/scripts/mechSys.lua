75ff09dcd3ac6b959c30548fba27fb7fec442761
ticidici <daniel.tomas@alum.upf.edu>
2018-09-20 20:45:49 +0200

    parte alrededor del mecanismo más vestida y con lógica

--- Bin/data/scripts/mechSys.lua
+++ Bin/data/scripts/mechSys.lua
@@ -12,3 +12,10 @@ function onMechanismSystemDeactivated_dani_mariosunshine_MS002()
   openDoor("dani_mariosunshine_MS002", 0)
 end
 
+function onMechanismSystemActivated_dani_mariosunshine_MS002()
+  closeDoor("dani_mariosunshine_MS002", 0)
+end
+function onMechanismSystemDeactivated_dani_mariosunshine_MS002()
+  openDoor("dani_mariosunshine_MS002", 0)
+end
+
