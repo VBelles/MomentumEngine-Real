4d2bcdcfd735a557a8773df1fa8beb5a0f0e4dfe
Daniel Tomas <ticidici@yahoo.es>
2018-09-25 01:30:50 +0200

    collider de roca cerca de la estatua arreglado, escena de jose pasada a dani

--- Bin/data/scripts/mechSys.lua
+++ Bin/data/scripts/mechSys.lua
@@ -1,15 +1,8 @@
-function onMechanismSystemActivated_dani_mariosunshine_MS002()
-  closeDoor("dani_mariosunshine_MS002", 0)
+function onMechanismSystemActivated_plataformeo_hex_mechsys_000()
+  closeDoor("plataformeo_hex_mechsys_000", 0)
 end
-function onMechanismSystemDeactivated_dani_mariosunshine_MS002()
-  openDoor("dani_mariosunshine_MS002", 0)
-end
-
-function onMechanismSystemActivated_dani_mariosunshine_MS002()
-  closeDoor("dani_mariosunshine_MS002", 0)
-end
-function onMechanismSystemDeactivated_dani_mariosunshine_MS002()
-  openDoor("dani_mariosunshine_MS002", 0)
+function onMechanismSystemDeactivated_plataformeo_hex_mechsys_000()
+  openDoor("plataformeo_hex_mechsys_000", 0)
 end
 
 function onMechanismSystemActivated_dani_mariosunshine_MS002()
