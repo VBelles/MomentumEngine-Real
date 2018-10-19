8a5d9ea5d5eed6c216569e371bbe38523b0822c4
VBelles <vicentbelles@gmail.com>
2018-10-19 20:58:17 +0200

    wip cinemáticas, añadidas las animaciones. Falta ajustar algunos tiempos de cámara.

--- Bin/data/scripts/autoload/levelStart.lua
+++ Bin/data/scripts/autoload/levelStart.lua
@@ -1,12 +1,18 @@
 function onLevelStart_game_state()
     [[
+    --teleportPlayer(-116.526, -22.974, 366.835) --Initial egg position.
+    --Start player animation.
+    setDummyState("cinematic_sleep_loop", true, 0, "cinematic_01")
+    changePlayerState("Dummy")
+
     --Start cinematicas
     wait(0.1)
     showHUD(false)
 	disablePlayerOutline()
 
-    local tiempoBarridoHorizonal = 2
-    local tiempoMuralVertical    = 8
+    local tiempoBarridoHorizonal = 0.01 --20
+    local tiempoMuralVertical    = 0.1 --10s para verse completo.
+    local blendTimeMural         = 0.001
     local tiempoIntroFloteProta  = 6
     local tiempoCenitalLevitar   = 8
     local tiempoGeneralSubida    = 8
@@ -14,42 +20,76 @@ function onLevelStart_game_state()
     local tiempoAterrizajeAndar  = 6
     local tiempoTravellingSalir  = 4
 
+    local fontSize = 1
+
     setPlatformEnabled("cine_cam_011", true)
 	blendInCamera("cine_cam_011", 0.001, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
     wait(tiempoBarridoHorizonal)
     
-    setPlatformEnabled("cine_cam_001", true)
-    blendInCamera("cine_cam_001", 0.001, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
-	wait(tiempoMuralVertical)
-    setPlatformEnabled("cine_cam_002", true)
-    blendInCamera("cine_cam_002", 0.001, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
-	wait(tiempoMuralVertical)
-    setPlatformEnabled("cine_cam_003", true)
-    blendInCamera("cine_cam_003", 0.001, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
-    wait(tiempoMuralVertical)
-    setPlatformEnabled("cine_cam_004", true)
-    blendInCamera("cine_cam_004", 0.001, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
-    wait(tiempoMuralVertical)
-    setPlatformEnabled("cine_cam_005", true)
-    blendInCamera("cine_cam_005", 0.001, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
-    wait(tiempoMuralVertical)
-    setPlatformEnabled("cine_cam_006", true)
-    blendInCamera("cine_cam_006", 0.001, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
-    wait(tiempoMuralVertical)
-    setPlatformEnabled("cine_cam_007", true)
-    blendInCamera("cine_cam_007", 0.001, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
-    wait(tiempoMuralVertical)
-    setPlatformEnabled("cine_cam_008", true)
-    blendInCamera("cine_cam_008", 0.001, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
-    wait(tiempoMuralVertical)
-    setPlatformEnabled("cine_cam_009", true)
-    blendInCamera("cine_cam_009", 0.001, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
-    wait(tiempoMuralVertical)
+    -- -- Start murales -- --
+        setPlatformEnabled("cine_cam_001", true)
+        blendInCamera("cine_cam_001", blendTimeMural, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
+        wait(tiempoMuralVertical)
+        showDialog("Historia 01", fontSize)
+        waitCondition(isDialogHidden)
 
+        setPlatformEnabled("cine_cam_002", true)
+        blendInCamera("cine_cam_002", blendTimeMural, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
+        wait(tiempoMuralVertical)
+        showDialog("Historia 02", fontSize)
+        waitCondition(isDialogHidden)
+
+        setPlatformEnabled("cine_cam_003", true)
+        blendInCamera("cine_cam_003", blendTimeMural, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
+        wait(tiempoMuralVertical)
+        showDialog("Historia 03", fontSize)
+        waitCondition(isDialogHidden)
+
+        setPlatformEnabled("cine_cam_004", true)
+        blendInCamera("cine_cam_004", blendTimeMural, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
+        wait(tiempoMuralVertical)
+        showDialog("Historia 04", fontSize)
+        waitCondition(isDialogHidden)
+
+        setPlatformEnabled("cine_cam_005", true)
+        blendInCamera("cine_cam_005", blendTimeMural, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
+        wait(tiempoMuralVertical)
+        showDialog("Historia 05", fontSize)
+        waitCondition(isDialogHidden)
+
+        setPlatformEnabled("cine_cam_006", true)
+        blendInCamera("cine_cam_006", blendTimeMural, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
+        wait(tiempoMuralVertical)
+        showDialog("Historia 06", fontSize)
+        waitCondition(isDialogHidden)
+
+        setPlatformEnabled("cine_cam_007", true)
+        blendInCamera("cine_cam_007", blendTimeMural, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
+        wait(tiempoMuralVertical)
+        showDialog("Historia 07", fontSize)
+        waitCondition(isDialogHidden)
+
+        setPlatformEnabled("cine_cam_008", true)
+        blendInCamera("cine_cam_008", blendTimeMural, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
+        wait(tiempoMuralVertical)
+        showDialog("Historia 08", fontSize)
+        waitCondition(isDialogHidden)
+
+        setPlatformEnabled("cine_cam_009", true)
+        blendInCamera("cine_cam_009", blendTimeMural, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
+        wait(tiempoMuralVertical)
+        showDialog("Historia 09", fontSize)
+        waitCondition(isDialogHidden)
+    -- -- End murales -- --
+    
     setPlatformEnabled("cine_cam_012", true)
     blendInCamera("cine_cam_012", 0.001, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
     wait(tiempoIntroFloteProta)
 
+    --Start main cinematic animation.
+    setDummyState("cinematic_01", false, 0, "cinematic_walk")
+    changePlayerState("Dummy")
+    
     setPlatformEnabled("cine_cam_013", true)
     blendInCamera("cine_cam_013", 0.001, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
     wait(tiempoCenitalLevitar)
@@ -59,7 +99,7 @@ function onLevelStart_game_state()
     wait(tiempoGeneralSubida)
 
     setPlatformEnabled("cine_cam_015", true)
-    blendInCamera("cine_cam_015", 0.001, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
+    blendInCamera("cine_cam_015", 2.000, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
     wait(tiempoGeneralSubidaTop)
     
     setPlatformEnabled("cine_cam_016", true)
@@ -68,6 +108,9 @@ function onLevelStart_game_state()
     
     setPlatformEnabled("cine_cam_010", true)
     blendInCamera("cine_cam_010", 0.001, CAMERA_PRIORITY_GAMEPLAY, INTERPOLATOR_CUBIC_OUT)
+    --Start cinematic walking animation.
+    setDummyState("cinematic_walk", true, 3, "idle")
+    changePlayerState("Dummy")
     wait(tiempoTravellingSalir)
     
     --Return to player.
@@ -78,7 +121,4 @@ function onLevelStart_game_state()
 	wait(0.5)
     --End cinematicas.
     ]]
-    showDialog("Welcome to momentum.\nPress accept to continue.", 1)
-    waitCondition(isDialogHidden)
-    showDialog("Have fun.", 1)
 end
